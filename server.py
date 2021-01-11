6a62f95c4f37fc5d409c1638c4ca8e23ebb2ce27
zed-chi <zed-chi@mail.ru>
2021-01-12 01:18:08 +0300

    upd

--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 from aiohttp import web
+import asyncio
+import datetime
 import aiofiles
 
+INTERVAL_SECS = 1
 
 async def archivate(request):
-    raise NotImplementedError
+    pass
 
 
 async def handle_index_page(request):
@@ -12,10 +15,33 @@ async def handle_index_page(request):
     return web.Response(text=index_contents, content_type='text/html')
 
 
+async def uptime_handler(request):
+    
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+
+        await asyncio.sleep(INTERVAL_SECS)
+
+
+
 if __name__ == '__main__':
     app = web.Application()
     app.add_routes([
         web.get('/', handle_index_page),
-        web.get('/archive/{archive_hash}/', archivate),
+        web.get('/archive/7kna/', uptime_handler),
+        #web.get('/archive/{archive_hash}/', archivate),        
     ])
     web.run_app(app)
