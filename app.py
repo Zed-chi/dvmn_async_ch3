ccb0f6190b4ff61f741725e717f9cd573be63aca
zed-chi <zed-chi@mail.ru>
2021-01-12 23:57:54 +0300

    black

--- app.py
+++ app.py
@@ -6,24 +6,28 @@ from zipfile import ZipFile
 
 
 def archivate():
-    process = subprocess.Popen(["bash", "zip_files.sh"], stdout=subprocess.PIPE)
+    process = subprocess.Popen(
+        ["bash", "zip_files.sh"], stdout=subprocess.PIPE
+    )
     sys.stdout.write(process.stdout.read())
+
 
 async def async_archivate(cmd=None):
     if not cmd:
         cmd = ["bash", "zip_files.sh"]
 
     proc = await asyncio.create_subprocess_exec(
-        "bash","zip_files.sh",
+        "bash",
+        "zip_files.sh",
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
+        stderr=asyncio.subprocess.PIPE,
     )
-    while True:        
+    while True:
         chunk = await proc.stdout.read(100000)
         if not chunk:
             break
-        sys.stdout.buffer.write(chunk)        
-    
+        sys.stdout.buffer.write(chunk)
+
 
 if __name__ == "__main__":
     asyncio.run(async_archivate())
